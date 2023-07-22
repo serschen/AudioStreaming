1a8b691ac3b80dd65dda29e4d33ab86057759ad4
Anton <anton.serschen@gmail.com>
2023-07-22 19:12:48 +0200

    fetches now songs from server and plays them when selected on a player activity

--- app/src/main/java/com/appdev/audiostreaming/Song.kt
+++ app/src/main/java/com/appdev/audiostreaming/Song.kt
@@ -1,5 +1,10 @@
 package com.appdev.audiostreaming
 
-class Song(val collection:String, val artist:String, val artistName: String, val path:String,
-           val name:String, val length:Int) {
+import android.os.Parcel
+import android.os.Parcelable
+
+class Song(
+    val collection: String?, val artist: String?, val artistName: String?, val path: String?,
+    val name: String?, val length:Int) {
+
 }
--- app/src/main/java/com/appdev/audiostreaming/SongAdapter.kt
+++ app/src/main/java/com/appdev/audiostreaming/SongAdapter.kt
@@ -1,15 +1,18 @@
 package com.appdev.audiostreaming
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class SongAdapter(private val songs:ArrayList<Song>) : RecyclerView.Adapter<SongAdapter.ViewHolder>() {
+class SongAdapter(private val songs:ArrayList<HashMap<String, Any>>) : RecyclerView.Adapter<SongAdapter.ViewHolder>() {
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val txtName: TextView? = itemView.findViewById(R.id.txtName)
         val txtArtist: TextView? = itemView.findViewById(R.id.txtArtist)
+        val searchResultLayout:LinearLayout = itemView.findViewById(R.id.searchResultLayout)
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.search_result, parent, false)
@@ -22,9 +25,17 @@ class SongAdapter(private val songs:ArrayList<Song>) : RecyclerView.Adapter<Song
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val song:Song = songs[position]
+        //val song:Song = songs[position]
 
-        holder.txtName?.text = song.name
-        holder.txtArtist?.text = song.artistName
+        //holder.txtName?.text = song.name
+        //holder.txtArtist?.text = song.artistName
+        holder.txtName?.text = songs[position]["name"].toString()
+        holder.txtArtist?.text = songs[position]["artistName"].toString()
+
+        holder.searchResultLayout.setOnClickListener{v ->
+            val intent = Intent(v.context, PlayerActivity::class.java)
+            intent.putExtra("map", songs[position])
+            v.context.startActivity(intent)
+        }
     }
 }
